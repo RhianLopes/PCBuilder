e9cbad04bbb3b81ec4119cfde248a793752f5590
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2022-02-15 10:42:58 -0300

    feat: finish pc build suggestion

--- src/main/java/br/com/ifsul/pcbuilder/service/pc/PCServiceImpl.java
+++ src/main/java/br/com/ifsul/pcbuilder/service/pc/PCServiceImpl.java
@@ -2,10 +2,14 @@ package br.com.ifsul.pcbuilder.service.pc;
 
 import br.com.ifsul.pcbuilder.dto.PCBuildingRequestDto;
 import br.com.ifsul.pcbuilder.dto.PCComponentSearchResponseDto;
+import br.com.ifsul.pcbuilder.model.Category;
 import br.com.ifsul.pcbuilder.service.pccomponent.PCComponentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -16,6 +20,17 @@ public class PCServiceImpl implements PCService {
 
     @Override
     public List<PCComponentSearchResponseDto> build(PCBuildingRequestDto requestDto) {
-        return pcComponentService.searchByCompatibility(requestDto.getCompatibility());
+        final List<PCComponentSearchResponseDto> filteredComponents = pcComponentService.searchByCompatibility(requestDto.getCompatibility());
+        final List<PCComponentSearchResponseDto> recommendedComponents = new ArrayList<>();
+        Arrays.asList(Category.values())
+            .forEach(category -> recommendedComponents.add(findFirstComponentsByCategoryAndOrderByPriceDesc(filteredComponents, category))
+        );
+        return recommendedComponents;
+    }
+
+    private PCComponentSearchResponseDto findFirstComponentsByCategoryAndOrderByPriceDesc(List<PCComponentSearchResponseDto> components, Category category) {
+        return components.stream()
+                .filter(component -> category.equals(component.getCategory())).min(Comparator.comparing(PCComponentSearchResponseDto::getPrice))
+                .orElse(null);
     }
 }
