a09e580398c9b9861114b13c036fcbb27c00175f
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2022-02-22 00:17:21 -0300

    Delete api/target/classes directory

--- api/target/classes/liquibase/v1/pccomponent/create-table-pccomponent.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE pc_component (
-    id              BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    brand           VARCHAR(255) NOT NULL,
-    model           VARCHAR(255) NOT NULL,
-    category        VARCHAR(255) NOT NULL,
-    price           DECIMAL(24, 2) NOT NULL,
-    compatibility   VARCHAR(255) NOT NULL,
-    PRIMARY KEY ( id )
-);
